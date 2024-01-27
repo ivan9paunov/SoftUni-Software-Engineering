deda5fe7a10b7612bd7bfc2f9a960d118e395b3e
ivan9paunov <vankatapaunov@gmail.com>
2024-01-28 00:50:40 +0200

    New exercise solved

--- JS-Advanced/05.DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ JS-Advanced/05.DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+    let input = document.getElementById('email');
+
+    input.addEventListener('change', noMatch);
+
+    function noMatch(event) {
+        let inputValue = event.target.value;
+        
+        input.classList.remove('error');
+        
+        if (!inputValue.match(pattern)) {
+            input.classList.add('error');
+        }
+        
+    }
 }
