f8b604810ceead4c796bbcf87e6845d57457af6d
ivan9paunov <vankatapaunov@gmail.com>
2024-01-29 01:45:11 +0200

    New exercise solved

--- JS-Advanced/05.Exercise - DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/05.Exercise - DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,45 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const container = Array.from(document.getElementById('main').children);
+    const encodeBtn = container[0].querySelector('button');
+    let encodeText = container[0].querySelector('textarea');
+    const decodeBtn = container[1].querySelector('button');
+    let decodeText = container[1].querySelector('textarea');
+
+    encodeBtn.addEventListener('click', encodeOperation);
+    decodeBtn.addEventListener('click', decodeOperation);
+
+    function encodeOperation(event) {
+        let encodedText = encodingText(encodeText.value);
+        decodeText.textContent = encodedText;
+        encodeText.value = '';
+    }
+
+    function decodeOperation(event) {
+        let decodedText = decodingText(decodeText.value);
+        decodeText.textContent = decodedText;
+    }
+
+    function encodingText(text) {
+        let transformedText = '';
+
+        for (let char of text) {
+            let charCode = char.charCodeAt() + 1;
+            let newChar = String.fromCharCode(charCode);
+            transformedText += newChar;
+        }
+
+        return transformedText;
+    }
+
+    function decodingText(text) {
+        let transformedText = '';
+
+        for (let char of text) {
+            let charCode = char.charCodeAt() - 1;
+            let newChar = String.fromCharCode(charCode);
+            transformedText += newChar;
+        }
+
+        return transformedText;
+    }
 }
