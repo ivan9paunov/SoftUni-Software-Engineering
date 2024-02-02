38dd63936302d43cc7575f078e39b17b0eb309ed
ivan9paunov <vankatapaunov@gmail.com>
2024-02-02 13:12:02 +0200

    Solution for task about Advanced Functions

--- JS-Advanced/06.Exercise - Advanced Functions/07. Simple Calculator/app.js
+++ JS-Advanced/06.Exercise - Advanced Functions/07. Simple Calculator/app.js
@@ -1,5 +1,20 @@
 function calculator() {
-    // TODO:
+    
+    let calculate = {
+        init(selector1, selector2, resultSelector) {
+            this.num1 = document.querySelector(selector1);
+            this.num2 = document.querySelector(selector2);
+            this.result = document.querySelector(resultSelector);
+        },
+        add() {
+            this.result.value = Number(this.num1.value) + Number(this.num2.value);
+        },
+        subtract() {
+            this.result.value = Number(this.num1.value) - Number(this.num2.value);
+        }
+    }
+
+    return calculate;
 }
 
 
