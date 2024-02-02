33441743fcb17cd42b2bf937eecf150fc67b526d
ivan9paunov <vankatapaunov@gmail.com>
2024-02-02 13:40:42 +0200

    Solution for task about Advanced Functions

--- JS-Advanced/06.Exercise - Advanced Functions/08. Next Article/script.js
+++ JS-Advanced/06.Exercise - Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,15 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let paragraphs = Array.from(articles);
+    const divContainer = document.getElementById('content');
+
+    return () => {
+        if (!paragraphs.length) {
+            return;
+        }
+
+        const article = paragraphs.shift();
+        const articleEl = document.createElement('article');
+        divContainer.appendChild(articleEl);
+        articleEl.textContent = article;
+    }
 }
