afb484a57d45fdf9debc0cfba889b228aba247aa
ivan9paunov <vankatapaunov@gmail.com>
2024-02-19 18:17:16 +0200

    Solution for task about DOM Manipulations and Events

--- JS-Advanced/05.Exercise - DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
+++ JS-Advanced/05.Exercise - DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,53 @@
 function solve() {
-  //TODO...
+    let rightAnswers = 0;
+
+    const [firstQuestion, secondQuestion, thirdQuestion] = document.querySelectorAll('section'); 
+    const resultText = document.querySelectorAll('h1')[1]; 
+    const [correctAnswer, wrongAnswer] = firstQuestion.querySelectorAll('p'); 
+
+    correctAnswer.addEventListener('click', passFirst);
+    wrongAnswer.addEventListener('click', passFirst);
+
+    function passFirst(event) {
+        if (event.target.textContent == 'onclick') {
+            rightAnswers++;
+        }
+
+        firstQuestion.style.display = 'none';
+        secondQuestion.style.display = 'block';
+
+        const [wrongAnswer, correctAnswer] = secondQuestion.querySelectorAll('p');
+
+        correctAnswer.addEventListener('click', passSecond);
+        wrongAnswer.addEventListener('click', passSecond);       
+    }
+
+    function passSecond(event) {
+        if (event.target.textContent == 'JSON.stringify()') {
+            rightAnswers++;
+        }
+
+        secondQuestion.style.display = 'none';
+        thirdQuestion.style.display = 'block';
+
+        const [correctAnswer, wrongAnswer] = thirdQuestion.querySelectorAll('p');
+
+        correctAnswer.addEventListener('click', passThird);
+        wrongAnswer.addEventListener('click', passThird);      
+    }
+
+    function passThird(event) {
+        if (event.target.textContent == 'A programming API for HTML and XML documents') {
+            rightAnswers++;
+        }
+
+        thirdQuestion.style.display = 'none';
+        document.getElementById('results').style.display = 'block';
+
+        if (rightAnswers == 3) {
+            resultText.textContent = 'You are recognized as top JavaScript fan!';
+        } else {
+            resultText.textContent = `You have ${rightAnswers} right answers`;
+        }
+    }
 }
