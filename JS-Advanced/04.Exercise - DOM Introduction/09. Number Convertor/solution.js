b89d688b01157891c6fca9157bbba2f1d8e29f21
ivan9paunov <vankatapaunov@gmail.com>
2024-02-20 12:09:06 +0200

    Solution for task about DOM Manipulations and Events

--- JS-Advanced/04.Exercise - DOM Introduction/09. Number Convertor/solution.js
+++ JS-Advanced/04.Exercise - DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    const convertTo = document.getElementById('selectMenuTo');
+    const convertBtn = document.querySelector('button');
+    const resultRef = document.getElementById('result');
+    
+    // creating the two options
+    convertTo.children[0].value = 'binary'; 
+    convertTo.children[0].textContent = 'Binary';
+    const hexOption = document.createElement('option');
+    hexOption.value = 'hexadecimal';
+    hexOption.textContent = 'Hexadecimal';
+    convertTo.appendChild(hexOption);
+    
+    convertBtn.addEventListener('click', convert);
 
-    //TODO...
+    function convert() {
+        const decimalNum = Number(document.getElementById('input').value);
+
+        if (convertTo.value == 'binary') {
+            resultRef.value = decimalNum.toString(2);
+        }
+
+        if (convertTo.value == 'hexadecimal') {
+            resultRef.value = decimalNum.toString(16).toUpperCase();
+        }
+    }
 }
