a3b29a865dec1178f7cd893987d837de67df3a57
ivan9paunov <vankatapaunov@gmail.com>
2024-04-01 23:35:32 +0300

    Solution for task about Introduction to TypeScript

---
+++ TypeScript/1. Introduction to TypeScript/dist/problem-1/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function calculateRectangleArea(a, b) {
+    const area = a * b;
+    console.log(area);
+}
+calculateRectangleArea(5, 7);
---
+++ TypeScript/1. Introduction to TypeScript/src/problem-1/app.ts
@@ -0,0 +1,6 @@
+function calculateRectangleArea(a: number, b: number): void {
+    const area: number = a * b;
+    console.log(area);
+}
+
+calculateRectangleArea(5, 7);
