d63c5547d1f16710a5aa3263a14f594f24470b16
ivan9paunov <vankatapaunov@gmail.com>
2024-04-13 23:57:57 +0300

    Solution for task about OOP

--- TypeScript/3. OOP/src/problem-6/app.ts
+++ TypeScript/3. OOP/src/problem-6/app.ts
@@ -22,14 +22,24 @@ class Pokemon {
 }
 
 type Trainers = {
-    [name: string]: Pokemon[];
+    [name: string]: TrainerCollection;
+}
+
+type TrainerCollection = {
+    badges: number;
+    collection: Pokemons[];
+}
+
+type Pokemons = {
+    name: string;
+    element: string;
+    health: number;
 }
 
 function pokemonTrainer(arr: string[]): void {
     const trainers: Trainers = {};
 
     let command: string | undefined = arr.shift();
-
 
     while (command != 'Tournament') {
         if (command != undefined) {
@@ -39,14 +49,55 @@ function pokemonTrainer(arr: string[]): void {
             const myPokemon = new Pokemon(pokemonName, element, health);
 
             if (trainers.hasOwnProperty(trainer)) {
-                trainers[trainer].push(myPokemon);
+                trainers[trainer].collection.push({ name: myPokemon.name, element: myPokemon.element, health: myPokemon.health });
             } else {
-                trainers[trainer] = [myPokemon];
+                trainers[trainer] = {
+                    badges: 0,
+                    collection: [{ name: myPokemon.name, element: myPokemon.element, health: myPokemon.health }]
+                };
             }
-            console.log(trainers);
         }
 
         command = arr.shift();
+    }
+
+    command = arr.shift();
+
+    while (command != 'End') {
+        for (const trainer in trainers) {
+            let notApply = true;
+
+            for (const pokemon of trainers[trainer].collection) {
+                if (pokemon.element == command) {
+                    trainers[trainer].badges++;
+                    notApply = false;
+                    break;
+                }
+            }
+
+            if (notApply) {
+                const collection = trainers[trainer].collection;
+
+                for (let i = 0; i < collection.length; i++) {
+                    let pokemon = collection[i];
+                    pokemon.health -= 10;
+
+                    if (pokemon.health <= 0) {
+                        collection.splice(i, 1);
+                    }
+                }
+            }
+        }
+
+        command = arr.shift();
+    }
+
+    const sortedTrainers = 
+        Object.entries(trainers)
+        .sort((a, b) => b[1].badges - a[1].badges);
+
+    for (let trainer of sortedTrainers) {
+        console.log(`${trainer[0]} ${trainer[1].badges} ${trainer[1].collection.length}`);
     }
 }
 
