87d3a2a79f4cd16af34fa0e07311a2b5792017e8
ivan9paunov <vankatapaunov@gmail.com>
2024-05-23 23:18:26 +0300

    Support adding breeds

--- JS-Back-End/01.Cat-Shelter-Workshop/src/handlers/addBread.js
+++ JS-Back-End/01.Cat-Shelter-Workshop/src/handlers/addBread.js
@@ -1,3 +1,4 @@
+const { addBreed } = require('../model.js');
 const { readTemplate, layout } = require('../util.js');
 
 async function addBreedHandler(req, res) {
@@ -13,13 +14,24 @@ async function postBreedHandler(req, res) {
     let data = '';
 
     req.on('data', (chunk) => data += chunk);
-    req.on('end', () => {
-        // Do something with data
-        console.log(new URLSearchParams(data));
-    });
+    req.on('end', async () => {
+        const formData = new URLSearchParams(data);
+        const breed = formData.get('breed');
 
-    res.statusCode = 204;
-    res.end();
+        if (breed) {
+            await addBreed(breed);
+
+            res.writeHead(301, [
+                'Location', '/'
+            ]);
+            res.end();
+        } else {
+            res.writeHead(301, [
+                'Location', '/cats/add-breed'
+            ]);
+            res.end();
+        }
+    });
 }
 
 module.exports = {
--- JS-Back-End/01.Cat-Shelter-Workshop/src/model.js
+++ JS-Back-End/01.Cat-Shelter-Workshop/src/model.js
@@ -23,7 +23,15 @@ async function getBreeds() {
     return data.breeds;
 }
 
+async function addBreed(breed) {
+    const data = await readData();
+    data.breeds.push(breed);
+
+    await writeData(data);
+}
+
 module.exports = {
     getCats,
-    getBreeds
+    getBreeds,
+    addBreed
 };
