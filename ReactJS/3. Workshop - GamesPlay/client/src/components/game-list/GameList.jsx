b8dfc646f6daaaa7051f41f5398bef23eff4d78d
ivan9paunov <vankatapaunov@gmail.com>
2024-07-22 00:07:00 +0300

    Add abstract requester and games api

--- ReactJS/3. Workshop - GamesPlay/client/src/components/game-list/GameList.jsx
+++ ReactJS/3. Workshop - GamesPlay/client/src/components/game-list/GameList.jsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+import * as gamesAPI from "../../api/games-api.js";
+
 export default function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gamesAPI.getAll()
+            .then(result => setGames(result));
+    }, []);
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
