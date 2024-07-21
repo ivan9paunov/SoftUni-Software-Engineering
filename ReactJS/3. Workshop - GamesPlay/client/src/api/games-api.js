e16f05fe88857539d28e0b9ef4d34244af7c13f2
ivan9paunov <vankatapaunov@gmail.com>
2024-07-22 00:46:54 +0300

    List all games

--- ReactJS/3. Workshop - GamesPlay/client/src/api/games-api.js
+++ ReactJS/3. Workshop - GamesPlay/client/src/api/games-api.js
@@ -2,4 +2,10 @@ import * as request from './requester.js';
 
 const BASE_URL = 'http://localhost:3030/jsonstore/games';
 
-export const getAll = () => request.get(BASE_URL);
+export const getAll = async () => {
+    const result = await request.get(BASE_URL);
+
+    const games = Object.values(result);
+
+    return games;
+}
---
+++ ReactJS/3. Workshop - GamesPlay/client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -0,0 +1,17 @@
+export default function GameListItem({
+    _id,
+    title,
+    category,
+    imageUrl
+}) {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={imageUrl} />
+                <h6>{category}</h6>
+                <h2>{title}</h2>
+                <a href="#" className="details-button">Details</a>
+            </div>
+        </div>
+    );
+}
