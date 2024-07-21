bcc107510757fdf800c974048cb11f09735b663e
ivan9paunov <vankatapaunov@gmail.com>
2024-07-21 23:18:18 +0300

    Add navigation links

--- ReactJS/3. Workshop - GamesPlay/client/src/App.jsx
+++ ReactJS/3. Workshop - GamesPlay/client/src/App.jsx
@@ -1,4 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
+import Login from './components/login/Login.jsx';
+import Register from './components/register/Register.jsx';
+import GameList from './components/game-list/GameList.jsx';
+import GameCreate from './components/game-create/GameCreate.jsx';
 
 function App() {
     return (
@@ -6,7 +13,13 @@ function App() {
             <Header />
 
             <main id="main-content">
-                
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/games' element={<GameList />} />
+                    <Route path='/games/create' element={<GameCreate />} />
+                </Routes>
             </main>
         </div>
     );
