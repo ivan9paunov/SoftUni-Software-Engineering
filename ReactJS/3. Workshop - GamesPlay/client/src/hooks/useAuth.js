ecaabdea25fb4cb4ba1b27eef6b03eb564275e43
ivan9paunov <vankatapaunov@gmail.com>
2024-07-26 01:59:20 +0300

    Add dynamic navigation and remove saved password in auth state

--- ReactJS/3. Workshop - GamesPlay/client/src/hooks/useAuth.js
+++ ReactJS/3. Workshop - GamesPlay/client/src/hooks/useAuth.js
@@ -7,11 +7,11 @@ export const useLogin = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const { password: _, authData } = await login(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
-        return result;
+        return authData;
     }
 
     return loginHandler;
@@ -21,11 +21,11 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const { password: _, ...authData } = await register(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
-        return result;
+        return authData;
     }
 
     return registerHandler;
