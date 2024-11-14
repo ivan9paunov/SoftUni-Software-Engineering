e27303fb43e80bd09791c781ea2f9522d2f21958
ivan9paunov <vankatapaunov@gmail.com>
2024-11-14 14:14:59 +0200

    Add header component

--- Angular/3. Workshop/workshop/src/app/app.component.ts
+++ Angular/3. Workshop/workshop/src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'workshop';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // this.http.get('https://angular-softuni-workshop-default-rtdb.firebaseio.com/workshop/.json').subscribe(x => {
+    //   console.log(x);
+    // });
+  }
 }
