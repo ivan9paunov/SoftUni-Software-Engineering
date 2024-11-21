910df2fac23bc922720e3c22eee61457214b50e3
ivan9paunov <vankatapaunov@gmail.com>
2024-11-21 14:09:37 +0200

    Create themes components

--- Angular/3. Workshop/workshop/src/app/posts-list/posts-list.component.ts
+++ Angular/3. Workshop/workshop/src/app/posts-list/posts-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../api.service';
-import { Post } from '../../types/post';
-import { LoaderComponent } from '../../shared/loader/loader.component';
+import { ApiService } from '../api.service';
+import { Post } from '../types/post';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
     selector: 'app-posts-list',
