02cbc3b1bb3ecdb7b9c952ac2d17197b1c77496b
ivan9paunov <vankatapaunov@gmail.com>
2024-11-27 23:59:43 +0200

    Get login errors

---
+++ Angular/3. Workshop/workshop/src/app/utils/email.validator.ts
@@ -0,0 +1,11 @@
+import { ValidatorFn } from "@angular/forms";
+
+export function emailValidator(domains: string[]): ValidatorFn {
+    const domainStr = domains.join('|');
+    const regExp = new RegExp(`[A-Za-z0-9]+@gmail\.(${domainStr})`);
+
+    return (control) => {
+        const isInvalid = control.value === '' || regExp.test(control.value);
+        return isInvalid ? null : { emailValidator: true };
+    };
+}
