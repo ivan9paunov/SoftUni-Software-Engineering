48b3ce9ef47a545075980c9ff42831a58c6e9fdc
ivan9paunov <vankatapaunov@gmail.com>
2024-11-30 00:07:46 +0200

    Handle 'create page' errors

--- Angular/3. Workshop/workshop/src/app/theme/add-theme/add-theme.component.ts
+++ Angular/3. Workshop/workshop/src/app/theme/add-theme/add-theme.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-theme',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-theme.component.html',
   styleUrl: './add-theme.component.css'
 })
@@ -12,12 +13,18 @@ export class AddThemeComponent {
 
   constructor(private apiService: ApiService) {}
 
-  addTheme(event: Event, themeName: string, postText: string) {
-    event.preventDefault();
+  addTheme(form: NgForm) {
+    console.log(form);
 
-    this.apiService.createTheme(themeName, postText).subscribe(data => {
-      console.log(data);
+    if (form.invalid) {
+      return;
+    }
+
+    console.log(form.value);
+
+    // this.apiService.createTheme(themeName, postText).subscribe(data => {
+    //   console.log(data);
       
-    });
+    // });
   }
 }
