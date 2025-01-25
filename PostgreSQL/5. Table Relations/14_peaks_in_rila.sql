bc493fde7f231dd78253cb8af9305a76817c6b85
ivan9paunov <vankatapaunov@gmail.com>
2025-01-25 14:15:56 +0200

    Join tables

---
+++ PostgreSQL/5. Table Relations/14_peaks_in_rila.sql
@@ -0,0 +1,15 @@
+SELECT 
+	mountain_range,
+	peak_name,
+	elevation
+FROM
+	mountains
+JOIN 
+	peaks
+ON 
+	peaks.mountain_id = mountains.id
+WHERE
+	mountain_range = 'Rila'
+ORDER BY
+	elevation DESC
+;
