3e60961004c03abd5b32e371b0f9594452ebeab8
ivan9paunov <vankatapaunov@gmail.com>
2025-01-31 22:51:25 +0200

    Create function for calculation

---
+++ PostgreSQL/7. Database Programmability and Transactions/02_user_defined_function_future_value.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION 
+	fn_calculate_future_value(
+		initial_sum DECIMAL, 
+		yearly_interest_rate DECIMAL,
+		number_of_years INT
+	)
+RETURNS
+	DECIMAL
+AS
+$$
+	DECLARE
+		future_value DECIMAL;
+	BEGIN
+		future_value := initial_sum * (POWER((1 + yearly_interest_rate), number_of_years));
+		RETURN TRUNC(future_value, 4);
+	END;
+$$
+LANGUAGE
+	plpgsql
+;
