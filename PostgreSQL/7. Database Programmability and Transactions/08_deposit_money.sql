c198652fe14e5505cbafdc0dc566c3858a83ab12
ivan9paunov <vankatapaunov@gmail.com>
2025-02-04 21:36:30 +0200

    Create procedure that updates records

---
+++ PostgreSQL/7. Database Programmability and Transactions/08_deposit_money.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PROCEDURE sp_deposit_money(
+	account_id INT,
+	money_amount NUMERIC(15, 4)
+)
+AS
+$$
+BEGIN
+	UPDATE 
+		accounts
+	SET 
+		balance = balance + money_amount
+	WHERE
+		id = account_id
+	;
+END;
+$$
+LANGUAGE
+	plpgsql
+;
