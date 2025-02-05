de603e2207d45609dc66b52db6acb885b6f4c99f
ivan9paunov <vankatapaunov@gmail.com>
2025-02-05 21:54:03 +0200

    Create database design

---
+++ PostgreSQL/8. Exam Preparation I/01_database_design.sql
@@ -0,0 +1,78 @@
+CREATE TABLE owners(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	phone_number VARCHAR(15) NOT NULL,
+	address VARCHAR(50)
+);
+
+CREATE TABLE animal_types(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	animal_type VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE cages(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	animal_type_id INT NOT NULL,
+
+	FOREIGN KEY (animal_type_id)
+	REFERENCES animal_types(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
+
+CREATE TABLE animals(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(30) NOT NULL,
+	birthdate DATE NOT NULL,
+	owner_id INT,
+	animal_type_id INT NOT NULL,
+
+	FOREIGN KEY (owner_id)
+	REFERENCES owners(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+
+	FOREIGN KEY (animal_type_id)
+	REFERENCES animal_types(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
+
+CREATE TABLE volunteers_departments(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE volunteers(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	phone_number VARCHAR(15) NOT NULL,
+	address VARCHAR(50),
+	animal_id INT,
+	department_id INT NOT NULL,
+
+	FOREIGN KEY (animal_id)
+	REFERENCES animals(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+
+	FOREIGN KEY (department_id)
+	REFERENCES volunteers_departments(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
+
+CREATE TABLE animals_cages(
+	cage_id INT NOT NULL,
+	animal_id INT NOT NULL,
+
+	FOREIGN KEY (cage_id)
+	REFERENCES cages(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+
+	FOREIGN KEY (animal_id)
+	REFERENCES animals(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
